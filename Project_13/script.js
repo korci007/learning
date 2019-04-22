3f6cc65671d194354ad3a0088914c3d88c4e65b5
Korci <korci007@gmail.com>
2019-04-22 22:56:25 +0200

    Project_13 update

--- Project_13/script.js
+++ Project_13/script.js
@@ -3,6 +3,7 @@ var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 var li = document.getElementsByTagName("li");
 var delButton = document.getElementsByClassName("btn_delete");
+var upButton = document.getElementsByClassName("btn_up");
 
 function inputLength() {
 	return input.value.length;
@@ -11,12 +12,16 @@ function inputLength() {
 function createListElement() {
 	var div = document.createElement("div");
 	var li = document.createElement("li");
-	var button = document.createElement("button");
-	button.appendChild(document.createTextNode("Delete"));
-	button.classList.add("btn_delete")
+	var button1 = document.createElement("button");
+	var button2 = document.createElement("button");
+	button2.appendChild(document.createTextNode("Delete"));
+	button2.classList.add("btn_delete")
+	button1.appendChild(document.createTextNode("Up"));
+	button1.classList.add("btn_up")
 	li.appendChild(document.createTextNode(input.value));
 	div.appendChild(li);
-	div.appendChild(button);
+	div.appendChild(button1);
+	div.appendChild(button2);
 	div.classList.add("myList");
 	ul.appendChild(div);
 	input.value = "";
@@ -36,21 +41,37 @@ function addListAfterKeypress(event) {
 }
 
 function toggleListElement() {
-	this.classList.toggle("done");
+	this.classList.toggle("done"); 
 }
 
 function deleteListElement() {
-	var parent = this.parentElement;
+	this.parentElement.remove();
+	/* var parent = this.parentElement;
 	var elementNumber = parent.childNodes.length -1;
 	for (i=elementNumber; i >= 0; i--) {
 		parent.removeChild(parent.childNodes[i]);
+	}; */
+}
+
+function upListElement() {
+	this.parentNode.classList.add("up");
+	var result = document.getElementsByClassName("up")[0].parentElement.children;
+	for (i=1; i< result.length; i++) {
+		if (result[i].className === "myList up") {
+			var temp = result[i].innerHTML;
+			result[i].innerHTML = result[i-1].innerHTML;
+			result[i-1].innerHTML = temp;
+		};
 	};
+	document.getElementsByClassName("up")[0].classList.remove("up");
+	createListeners();
 }
 
 function createListeners() {
 	for (i=0; i < li.length; i++) {
 		li[i].addEventListener("click", toggleListElement); 
-		delButton[i].addEventListener("click", deleteListElement); 
+		delButton[i].addEventListener("click", deleteListElement);
+		upButton[i].addEventListener("click", upListElement); 
 	};
 	button.addEventListener("click", addListAfterClick);
 	input.addEventListener("keypress", addListAfterKeypress);
